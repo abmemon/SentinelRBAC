260516c5b69aef7bde38981647b8740372ce976f
A B Memon <56878874+abmemon@users.noreply.github.com>
2025-04-06 01:15:16 +0500

    Code fix SentinelRbacServiceProvider

--- src/Providers/SentinelRbacServiceProvider.php
+++ src/Providers/SentinelRbacServiceProvider.php
@@ -5,7 +5,7 @@ namespace ABMemon\SentinelRBAC\Providers;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-use App\Models\Permission;
+use ABMemon\SentinelRBAC\Permission;
 
 class SentinelRbacServiceProvider extends ServiceProvider
 {
