9d78ba7339ad226a8852762eef82951d97cdd6ba
A B Memon <56878874+abmemon@users.noreply.github.com>
2025-04-08 19:09:16 +0500

    Update config/sentinelrbac.php

--- config/sentinelrbac.php
+++ config/sentinelrbac.php
@@ -1,5 +1,57 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Model
+    |--------------------------------------------------------------------------
+    |
+    | The Eloquent user model used by your application.
+    |
+    */
     'user_model' => App\Models\User::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Model Bindings
+    |--------------------------------------------------------------------------
+    |
+    | You may override the default models used internally.
+    |
+    */
+    'models' => [
+        'role' => ABMemon\SentinelRBAC\Models\Role::class,
+        'permission' => ABMemon\SentinelRBAC\Models\Permission::class,
+        'group' => ABMemon\SentinelRBAC\Models\Group::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Table Names
+    |--------------------------------------------------------------------------
+    */
+    'table_names' => [
+        'roles' => 'roles',
+        'permissions' => 'permissions',
+        'groups' => 'groups',
+        'role_user' => 'role_user',
+        'permission_user' => 'permission_user',
+        'group_user' => 'group_user',
+        'permission_role' => 'permission_role',
+        'group_permission' => 'group_permission',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Settings
+    |--------------------------------------------------------------------------
+    |
+    | These settings control caching of permissions per user.
+    |
+    */
+    'cache' => [
+        'enabled' => true,
+        'expiration_minutes' => 60,
+    ],
 ];
